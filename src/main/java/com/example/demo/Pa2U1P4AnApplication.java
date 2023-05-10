6283c8c970093ea3eee6f940e3ec16f0d941472d
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-05-09 19:31:56 -0500

    mi primera subida

---
+++ src/main/java/com/example/demo/Pa2U1P4AnApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U1P4AnApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U1P4AnApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("mi primer proyecto");
+		
+	}
+
+}
