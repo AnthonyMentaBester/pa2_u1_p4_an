6922385d916a00441ee38c15667b0be34d477bd9
Anthony Narvaez <ajnarvaezr@uce.edu.ec>
2023-05-19 19:42:44 -0500

    Taller5 repository

---
+++ src/main/java/com/example/demo/repository/EstudianteRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import com.example.demo.repository.modelo.Estudiante;
+
+public interface EstudianteRepository {
+	public void insertar(Estudiante estudiante);
+
+	public void actualizar(Estudiante estudiante);
+
+	// se busca apartir de un identificador o primary key
+	public Estudiante seleccionar(String cedula);
+
+	// tambien se elimina por un identificador
+	public void eliminar(String cedula);
+
+}
